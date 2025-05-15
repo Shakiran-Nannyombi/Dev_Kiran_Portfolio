343554ec39284414130c1a198a3fca873d845440
shakiran-nannyombi <devkiran256@gmail.com>
2025-05-16 00:43:06 +0300

    improved structure of my app

--- Kiran_Portfolio/src/main.js
+++ Kiran_Portfolio/src/main.js
@@ -1,15 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { createPinia } from 'pinia'
-import './assets/styles/main.css'
+import './style.css'
 
-// Create app instance
 const app = createApp(App)
-
-// Use plugins
-app.use(createPinia())
 app.use(router)
-
-// Mount app
 app.mount('#app')
